0c6af731066354acf6a1e208320de78839c420aa
AndrewLinus24 <46133532+AndrewLinus24@users.noreply.github.com>
2019-12-15 16:24:38 +0700

    Add files via upload

--- ACO/main.py
+++ ACO/main.py
@@ -2,12 +2,22 @@ from source import ACO
 import numpy
 
 MINIMUM_DISTANCE = {
-    "10kota": 50,
-    "eil51con": 426,
-    "berlin52conv": 1000,
+    "10kota": 48,
+    "eil51": 426,
+    "berlin52": 7544,
+}
+
+NUMBER_OF_CITY = {
+    "10kota": 10,
+    "eil51": 51,
+    "berlin52": 52,
 }
 
 def load_dataset(filename):
+    """
+    Load dataset from .txt and save it into array.
+    Dataset format: (city_x_coordinate)\t(city_y_coordinate)\n
+    """
     file = open(filename, "r")
     iter_file = iter(file)
     next(iter_file)
@@ -19,31 +29,77 @@ def load_dataset(filename):
     file.close()
     return(array)
 
-def set_distance_matrix(dataset):
+def set_distance_matrix(dataset, inversed = False):
+    """
+    Set distance matrix (2D numpy.array) based on array of dataset.
+    Distance is calculated using Euclidean Distance equation:
+        numpy.linalg.norm(numpy.array(start_city) - numpy.array(end_city))
+    Diagonal element is set to zero.
+    If inversed = True, distance matrix elements are multiplicatively inversed:
+        5 becomes 1/5
+        0 remains 0
+    """
     array = []
     for start_city in dataset:
+        array_row = []
         for end_city in dataset:
             if(start_city != end_city):
-                array.append(numpy.linalg.norm(numpy.array(start_city) - numpy.array(end_city)))
+                if(inversed):
+                    array_row.append(1 / numpy.linalg.norm(numpy.array(start_city) - numpy.array(end_city)))
+                else:
+                    array_row.append(numpy.linalg.norm(numpy.array(start_city) - numpy.array(end_city)))
             else:
-                array.append(0)
+                array_row.append(0)
+        array.append(array_row)
     return numpy.array(array)
 
 def set_pheromone_matrix(dataset):
+    """
+    Set pheromone matrix (2D numpy.array) based on array of dataset.
+    Pheromone is calculated using this equation:
+        1 / MINIMUM_DISTANCE
+    Diagonal element is set to zero.
+    """
     array = []
     for start_city in dataset:
+        array_row = []
         for end_city in dataset:
             if(start_city != end_city):
-                array.append(1/MINIMUM_DISTANCE["10kota"])
+                if(len(dataset) == NUMBER_OF_CITY["10kota"]):
+                    array_row.append(1 / MINIMUM_DISTANCE["10kota"])
+                elif(len(dataset) == NUMBER_OF_CITY["eil51"]):
+                    array_row.append(1 / MINIMUM_DISTANCE["eil51"])
+                elif(len(dataset) == NUMBER_OF_CITY["berlin52"]):
+                    array_row.append(1 / MINIMUM_DISTANCE["berlin52"])
             else:
-                array.append(0)
+                array_row.append(0)
+        array.append(array_row)
     return numpy.array(array)
 
-def run_10kota():
-    dataset = load_dataset("10kota.txt")
+def run_aco(dataset_name, default_parameter):
+    """
+    Run ACO algorithm for calculating shortest path.
+    Parameters:
+        dataset_name (string)
+            Name of dataset that is being used.
+            Choices: "10kota", "eil51", "berlin52".
+        default_parameter (boolean)
+            True: Using default parameter.
+            False: Using custom parameter.
+    """
+    dataset = load_dataset(dataset_name + ".txt")
+    print("Dataset " + dataset_name)
+    print(dataset)
+    print()
+
     distance_matrix = set_distance_matrix(dataset)
-    print("Distance Matrix (Multiplicatively Inversed)")
+    print("Distance Matrix")
     print(distance_matrix)
+    print()
+
+    inversed_distance_matrix = set_distance_matrix(dataset, inversed = True)
+    print("Distance Matrix (Inversed Element)")
+    print(inversed_distance_matrix)
     print()
 
     pheromone_matrix = set_pheromone_matrix(dataset)
@@ -51,41 +107,119 @@ def run_10kota():
     print(pheromone_matrix)
     print()
 
-    print("ACO Shortest Distance: ")
+    number_of_ant = NUMBER_OF_CITY[dataset_name]
+    number_of_iter = 1000
+    decay = 0.5
+    alpha = 1
+    beta = 1
+    q = 100
+
+    if(not default_parameter):
+        number_of_ant = 0
+        while(number_of_ant < 1):
+            number_of_ant = input("Input number of ant [> 0] [default = number of city]: ")
+            try:
+                number_of_ant = int(number_of_ant)
+            except:
+                number_of_ant = 0
+                print("Input must be integer!")
+                pass
+        
+        number_of_iter = 0
+        while(number_of_iter < 1):
+            number_of_iter = input("Input number of iteration [> 0] [default = 1000]: ")
+            try:
+                number_of_iter = int(number_of_iter)
+            except:
+                number_of_iter = 0
+                print("Input must be integer!")
+                pass
+
+        decay = -1
+        while(decay < 0 or decay >= 1):
+            decay = input("Input pheromone decay rate [>= 0.00] [< 1.00] [default = 0.5]: ")
+            try:
+                decay = float(decay)
+            except:
+                decay = -1
+                print("Input must be integer or decimal number!")
+                pass
+
+        alpha = -1
+        while(alpha < 0):
+            alpha = input("Input alpha (weight of pheromone) [>= 0.00] [default = 1.00]: ")
+            try:
+                alpha = float(alpha)
+            except:
+                alpha = -1
+                print("Input must be integer or decimal number!")
+                pass
+
+        beta = -1
+        while(beta < 0):
+            beta = input("Input beta (weight of distance) [>= 0.00] [default = 1.00]: ")
+            try:
+                beta = float(beta)
+            except:
+                beta = -1
+                print("Input must be integer or decimal number!")
+                pass
+
+        q = 0
+        while(q < 1):
+            q = input("Input q (dividend for updating pheromone) [>= 1.00] [default = 100]: ")
+            try:
+                q = float(q)
+            except:
+                q = 0
+                print("Input must be integer or decimal number!")
+                pass
+        
+    aco = ACO(distance_matrix, inversed_distance_matrix, pheromone_matrix, number_of_ant, number_of_iter, decay, alpha, beta, q)
+    result = aco.calculate_shortest_path()
     print()
-
-    print("ACO Shortest Path: ")
-
-def run_eil51con():
-    pass
-
-def run_berlin52_conv():
-    pass
+    print("Shortest Path: {}".format(result[0]))
+    print("Shortest Distance: {}".format(result[1]))
 
 def print_developer():
-    print("This app was developed by:")
-    print("Andrew - 2201917126")
-    print("Adithya Nugraha Tjokrosetio - 2001551315")
-    print("David William - 2001540066")
-
-def clearScreen(x):
-    for i in range (0, x):
-        print()
+    """
+    Print developer information.
+    """
+    print("This application was developed by:")
+    print("2201916956 - David William")
+    print("2201917095 - Adithya Nugraha Tjokrosetio")
+    print("2201917126 - Andrew")
 
 def main():
+    """
+    Main function.
+    """
     index = 0
-    while(index != 5):
-        clearScreen(25)
+    while(index != 8):
+        for i in range (0, 25):
+            print()
         index = 0
+        print("Default Parameter")
+        print("=================")
+        print("Number of ant = Number of city")
+        print("Number of iteration = 1000")
+        print("Pheromone decay rate = 0.5")
+        print("Alpha (weight of pheromone) = 1")
+        print("Beta (weight of distance) = 1")
+        print("Q (dividend for updating pheromone) = 100")
+        print()
         print("Ant Colony Optimization (ACO)")
         print("=============================")
-        print("1. 10kota")
-        print("2. eil51con")
-        print("3. berlin52conv")
-        print("4. About Developer")
-        print("5. Exit")
-        while(index < 1 or index > 5):
-            index = input("Input [1-5]: ")
+        print("1. 10kota - Default Parameter")
+        print("2. 10kota - Custom Parameter")
+        print("3. eil51 - Default Parameter")
+        print("4. eil51 - Custom Parameter")
+        print("5. berlin52 - Default Parameter")
+        print("6. berlin52 - Custom Parameter")
+        print("7. About Developer")
+        print("8. Exit")
+        while(index < 1 or index > 8):
+            index = input("Input [1-8]: ")
             try:
                 index = int(index)
             except:
@@ -94,14 +228,23 @@ def main():
                 pass
         print()
         if(index == 1):
-            run_10kota()
+            run_aco(dataset_name = "10kota", default_parameter = True)
         elif(index == 2):
-            run_eil51con()
+            run_aco(dataset_name = "10kota", default_parameter = False)
         elif(index == 3):
-            run_berlin52_conv()
+            run_aco(dataset_name = "eil51", default_parameter = True)
         elif(index == 4):
+            run_aco(dataset_name = "eil51", default_parameter = False)
+        elif(index == 5):
+            run_aco(dataset_name = "berlin52", default_parameter = True)
+        elif(index == 6):
+            run_aco(dataset_name = "berlin52", default_parameter = False)
+        elif(index == 7):
             print_developer()
-        if(index != 5):
+        if(index != 8):
+            print()
+            print("Press ENTER to continue...")
+            print()
             input()
 
 main()
--- ACO/source.py
+++ ACO/source.py
@@ -1,3 +1,120 @@
+import numpy
+
 class ACO:
-    def __init__(self):
-        pass
+    def __init__(self, distance_matrix, inversed_distance_matrix, pheromone_matrix, number_of_ant, number_of_iter, decay, alpha, beta, q):
+        """
+        Parameters:
+            distance_matrix (2D numpy.array)
+                Distance matrix.
+            inversed_distance_matrix (2D numpy.array)
+                Distance matrix whose elements are multiplicatively inversed.
+            pheromone_matrix (2D numpy.array)
+                Pheromone matrix.
+            number_of_ant (int)
+                Number of ant.
+                Default: Equals to number of city.
+            number_of_iter (int)
+                Number of iteration (1 <= number_of_iter).
+                Default: 1000.
+            decay (float)
+                Pheromone decay rate (0 <= decay < 1).
+                The higher decay rate, the faster pheromone decay.
+                Default: 0.5.
+            alpha (float)
+                Weight of pheromone (0 <= alpha).
+                Default: 1.
+            beta (float)
+                Weight of distance (0 <= beta).
+                Default: 1.
+            q (int)
+                Dividend for updating pheromone (0 < q).
+                Default: 100.
+            all_move (range)
+                All index representing move between 2 cities.
+        """
+        self.distance_matrix = distance_matrix
+        self.inversed_distance_matrix = inversed_distance_matrix
+        self.pheromone_matrix = pheromone_matrix
+        self.number_of_ant = number_of_ant
+        self.number_of_iter = number_of_iter
+        self.decay = decay
+        self.alpha = alpha
+        self.beta = beta
+        self.q = q
+        self.all_move = range(len(distance_matrix))
+
+    def calculate_shortest_path(self):
+        """
+        Calculate and return shortest path using ACO algorithm.
+        """
+        shortest_path = None
+        shortest_shortest_path = ("placeholder", numpy.inf)
+        for i in range(self.number_of_iter):
+            all_path = self.generate_all_path()
+            self.update_pheromone(all_path)
+            shortest_path = min(all_path, key = lambda x: x[1])
+            print("Iteration " + str(i))
+            print(shortest_path)
+            print()
+            if(shortest_path[1] < shortest_shortest_path[1]):
+                shortest_shortest_path = shortest_path
+        return shortest_shortest_path
+    
+    def generate_all_path(self):
+        """
+        Generate and return all path (array of sets).
+        """
+        all_path = []
+        for i in range(self.number_of_ant):
+            path = self.generate_path(i)
+            all_path.append((path, self.calculate_path_distance(path)))
+        return all_path
+
+    def generate_path(self, start_city):
+        """
+        Generate and return path (array of sets).
+        """
+        path = []
+        city_visited = set()
+        city_visited.add(start_city)
+        current_city = start_city
+        for i in range(len(self.distance_matrix) - 1):
+            next_city = self.select_next_city(self.pheromone_matrix[current_city], self.inversed_distance_matrix[current_city], city_visited)
+            path.append((current_city, next_city))
+            current_city = next_city
+            city_visited.add(next_city)
+        path.append((current_city, start_city))
+        return path
+
+    def select_next_city(self, pheromone, inversed_distance, city_visited):
+        """
+        Select and return next move based on probability (int).
+        """
+        pheromone = numpy.copy(pheromone)
+        pheromone[list(city_visited)] = 0
+
+        move_probability = (pheromone ** self.alpha) * (inversed_distance ** self.beta)
+        move_probability = move_probability / move_probability.sum()
+
+        next_city = numpy.random.choice(self.all_move, 1, p = move_probability)[0]
+        return next_city
+
+    def calculate_path_distance(self, path):
+        """
+        Calculate and return distance of a path (int).
+        """
+        distance = 0
+        for city in path:
+            distance += self.distance_matrix[city]
+        return distance
+
+    def update_pheromone(self, all_path):
+        """
+        Update pheromone based on decay and move.
+        """
+        sorted_path = sorted(all_path, key = lambda x: x[1])
+        self.pheromone_matrix = (1 - self.decay) * self.pheromone_matrix
+        for path, _ in sorted_path:
+            for move in path:
+                self.pheromone_matrix[move] += self.q / self.distance_matrix[move]
+        
